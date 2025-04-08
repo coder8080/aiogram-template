a5dc2c6cd40b28379cf50b9d77e055eaf54dd6ad
Roman Kolerov <coder8080@yandex.ru>
2025-04-08 13:48:59 +0300

    Remove alembic configs (just use exec inside dev container)

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import asyncio
 from aiogram import Dispatcher
 from aiohttp import ClientSession
 
-from common.environment import get_str_env
+from common.environment import get_bool_env, get_str_env
 from src.bot.start.router import router as start_router
 from src.bot.user.middlewares import GetUserMiddleware
 from src.common.bot import bot
@@ -21,20 +21,22 @@ async def main():
     session = ClientSession()
 
     POD_NAME = get_str_env("POD_NAME")
+    LOCAL = get_bool_env("LOCAL")
 
-    print("⏱️ Waiting to become main instance...")
+    if not LOCAL:
+        print("⏱️ Waiting to become main instance...")
 
-    while True:
-        try:
-            response = await session.get("http://bot-podname/pod_name")
-            json = await response.json()
-            assert json["pod_name"] == POD_NAME
-            await asyncio.sleep(0.05)
-            break
-        except Exception:
-            pass
+        while True:
+            try:
+                response = await session.get("http://bot-podname/pod_name")
+                json = await response.json()
+                assert json["pod_name"] == POD_NAME
+                await asyncio.sleep(0.05)
+                break
+            except Exception:
+                pass
 
-    print("✅ Pod has become main instance. Starting polling")
+        print("✅ Pod has become main instance. Starting polling")
 
     await dp.start_polling(bot)
 
